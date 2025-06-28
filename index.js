2f91796b463a659a23e580940de2b2be5852e77b
SALEH-abdulwahab <saleh200228@gmail.com>
2025-06-28 23:06:34 +0300

    cm

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({ name: name, email: email })
+  })
+    .then(response => response.json())
+    .then(data => {
+      const newId = data.id;
+      const p = document.createElement("p");
+      p.textContent = newId;
+      document.body.appendChild(p);
+    })
+    .catch(error => {
+      const errorMsg = document.createElement("p");
+      errorMsg.textContent = error.message;
+      document.body.appendChild(errorMsg);
+    });
+}
